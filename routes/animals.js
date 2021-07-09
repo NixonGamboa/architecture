26265084aca2b8c7531e3b49a8d352aa12417e33
Nixon Gamboa <nrgamboa97@gmail.com>
2021-07-08 22:54:26 -0500

    routes for 5 first controllers created

--- routes/animals.js
+++ routes/animals.js
@@ -16,5 +16,50 @@ function animalsApi(app) {
       next(err);
     }
   });
+  router.get('/:animalId', async (req, res, next) => {
+    try {
+      const animal = await Promise.resolve(animalsMock[0]);
+      res.status(200).json({
+        data: animal,
+        message: 'animal retrived',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  router.post('/', async (req, res, next) => {
+    try {
+      const createdAnimalId = await Promise.resolve(animalsMock[0].id);
+      res.status(201).json({
+        data: createdAnimalId,
+        message: 'animal created',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  router.put('/:animalId', async (req, res, next) => {
+    try {
+      const updatedAnimalId = await Promise.resolve(animalsMock[0].id);
+      res.status(200).json({
+        data: updatedAnimalId,
+        message: 'animal updated',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  router.delete('/:animalId', async (req, res, next) => {
+    try {
+      const deletedAnimalId = await Promise.resolve(animalsMock[0].id);
+      res.status(200).json({
+        data: deletedAnimalId,
+        message: 'animal deleted',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
 }
 module.exports = animalsApi;
